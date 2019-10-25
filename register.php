311ac42523b0188c9809898ca925ca3aa8ee0115
Susan Jang <sj7yj@virginia.edu>
2019-10-25 16:56:23 -0400

    fixed the error/ added functions of checking if the password1 and password2 are eqal & checking if one of the field is empty

--- register.php
+++ register.php
@@ -2,18 +2,66 @@
  include_once("./library.php"); // To connect to the database
  $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
  // Check connection
+
+//error array
+ $errors = array(); 
+
+
+//  form validation: ensure that the form is correctly filled ...
+//   by adding (array_push()) corresponding error unto $errors array
+//   if (empty($_POST[computingID])) { array_push($errors, "computingID is required"); }
+//   if (empty($_POST[major])) { array_push($errors, "major is required"); }
+//   if (empty($_POST[password_1])) { array_push($errors, "Password is required"); }
+//   if ($password_1 != $password_2) {
+// 	array_push($errors, "The two passwords do not match");
+//   }
+
+// if (count($errors) > 0) {
+// 	foreach ($errors as $error) :
+// 		echo $error	
+// }
+
  if (mysqli_connect_errno())
  {
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
  }
+
+//check if the password and re-entered password is same
+ if ($_POST[password_1] != $_POST[password_2]) {
+	echo "The two passwords do not match";
+	mysqli_close($con);
+  } 
+
+  //check if the input field is not empty --> put up an error message if one of the field is empty
+  	
+  if (empty($_POST[computingID])) { 
+  	echo "computingID is required";
+  	mysqli_close($con);
+   }
+  
+  if (empty($_POST[major])) { 
+  	echo "Major is required";
+  	mysqli_close($con);
+   }
+   if (empty($_POST[password_1])) { 
+  	echo "passwords is required";
+  	mysqli_close($con);
+   }
+   if (empty($_POST[password_2])) { 
+  	echo "Reenter password";
+  	mysqli_close($con);
+   }
+  
+
  // Form the SQL query (an INSERT query)
  $sql="INSERT INTO Users (computingID, major, password)
  VALUES
- ('$_POST[computingID]','$_POST[major]','$_POST[password]')";
+ ('$_POST[computingID]','$_POST[major]','$_POST[password_1]')";
+
 
  if (!mysqli_query($con,$sql))
  {
- die('Error: ' . mysqli_error($con));
+ die( mysqli_error($con));
  }
  echo "1 record added"; // Output to user
  mysqli_close($con);
