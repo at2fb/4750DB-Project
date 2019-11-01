1c12e5e00570713930ab1b92eef0a026ddbdd200
Aaron <at2fb@virginia.edu>
2019-11-01 12:53:03 -0400

    Login Working

--- login.php
+++ login.php
@@ -13,12 +13,12 @@
 //   if (empty($_POST[major])) { array_push($errors, "major is required"); }
 //   if (empty($_POST[password_1])) { array_push($errors, "Password is required"); }
 //   if ($password_1 != $password_2) {
-// 	array_push($errors, "The two passwords do not match");
+//  array_push($errors, "The two passwords do not match");
 //   }
 
 // if (count($errors) > 0) {
-// 	foreach ($errors as $error) :
-// 		echo $error	
+//  foreach ($errors as $error) :
+//    echo $error 
 // }
 
  if (mysqli_connect_errno())
@@ -27,22 +27,20 @@
  }
 
   //check if the input field is not empty --> put up an error message if one of the field is empty
-  	
+    
   if (empty($_POST[computingID])) { 
-  	echo "computingID is required";
-  	mysqli_close($con);
+    echo "computingID is required";
+    mysqli_close($con);
    }
   
    if (empty($_POST[password_1])) { 
-  	echo "passwords is required";
-  	mysqli_close($con);
+    echo "passwords is required";
+    mysqli_close($con);
    }
 
 
  // Form the SQL query (an INSERT query)
- $sql="SELECT computingID, password FROM Users WHERE computingID = $_POST[user] AND password = $_POST[password_1]
- VALUES
- ('$_POST[computingID]','$_POST[major]','$_POST[password_1]')";
+ $sql="SELECT computingID, password FROM Users WHERE computingID = '$_POST[computingID]' AND password = '$_POST[password_1]'";
 
 
  if (!mysqli_query($con,$sql))
